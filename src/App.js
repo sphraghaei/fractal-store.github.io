54eed5fcb91f2bc34d21b32653cc3c66213e5a60
sphraghaei <86032246+sphraghaei@users.noreply.github.com>
2021-10-26 10:01:31 +0330

    first update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className='App'>
       <Navbar bg='dark'>
         <Navbar.Brand>
-          Logo
+          Logo app
         </Navbar.Brand>
       </Navbar>
     </div>
